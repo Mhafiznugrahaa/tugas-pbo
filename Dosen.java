98273ce5df77c4cc3b298901a3d315098157a9aa
ipzoone <saifalimusy@gmail.com>
2025-09-17 10:50:36 +0700

    Saif Ali Mushaddiq

---
+++ Dosen.java
@@ -0,0 +1,23 @@
+public class Dosen {
+    String nama;
+    String nip;
+    String alamat;
+    String email;
+    int noTelp;
+
+    public Dosen(String nama, String nip, String alamat, String email, int noTelp) {
+        this.nama = nama;
+        this.nip = nip;
+        this.alamat = alamat;
+        this.email = email;
+        this.noTelp = noTelp;
+    }
+
+    public void tampilkanDosen() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIP: " + nip);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Email: " + email);
+        System.out.println("No. Telp: " + noTelp);
+    }
+}
